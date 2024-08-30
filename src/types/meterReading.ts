7ded033c1fe24e764109babce9d3c6a9d9b10651
Leandro <leandromllr9@gmail.com>
2024-08-29 23:00:16 -0300

    feat: add Measurement and related interfaces for request and response handling

--- src/types/meterReading.ts
+++ src/types/meterReading.ts
@@ -13,3 +13,22 @@ export interface Measure{
   image_url: string;
   customer_code: string;
 }
+
+export interface ConfirmMeterReadingRequest {
+  measure_uuid: string;
+  confirmed_value: number
+}
+
+export interface Measurement {
+  measure_uuid: string;
+  measure_datetime: Date;
+  measure_type: string;
+  has_confirmed: boolean;
+  image_url: string;
+}
+
+export interface MeasurementResponse {
+  customer_code: string;
+  measures: Measurement[];
+}
+
