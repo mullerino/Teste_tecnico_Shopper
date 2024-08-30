d3c27bf1c4786dbac3cf587f9a2ba3183ef05d8e
Leandro <leandromllr9@gmail.com>
2024-08-29 23:06:41 -0300

    chore: initialize dotenv for environment variable management

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, {Application} from 'express'
 import routes from './routes/meterReadingRoutes'
+import dotenv from 'dotenv'
 
 function main () {
   const app : Application = express()
   const port = 3000
+  
+  dotenv.config()
 
   app.use(express.json({ limit: '50mb' }))
   app.use(express.urlencoded({ limit: '50mb', extended: true }))
