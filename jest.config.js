6ebb9bec4ceede0aaac34bbd98736cb889bd7e60
Leandro <leandromllr9@gmail.com>
2024-08-30 20:42:18 -0300

    chore: update jest configuration for TypeScript support

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleDirectories: ['node_modules', 'src'],
+};
