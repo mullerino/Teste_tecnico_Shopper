034a92d375b22e8ff164e9d6006c0e9203475b59
Leandro <leandromllr9@gmail.com>
2024-08-30 20:40:38 -0300

    refactor: refactor express app initialization to return server instance for better testability

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import express, {Application} from 'express'
+import express, { Application } from 'express'
 import { cfg } from './config/config'
 import routes from './routes/measureRoutes'
 import dotenv from 'dotenv'
 
-function main () {
-  const app : Application = express()
-  
+const app: Application = express()
+
+function main() {
   dotenv.config()
 
   app.use(express.json({ limit: '50mb' }))
   app.use(express.urlencoded({ limit: '50mb', extended: true }))
   app.use(routes)
-  
-  app.listen(cfg.server.port, () => {
+
+  const server = app.listen(cfg.server.port, () => {
     console.log(`Servidor rodando na porta ${cfg.server.port}`)
   })
+
+  return server
 }
 
-main()
+export const server = main()
